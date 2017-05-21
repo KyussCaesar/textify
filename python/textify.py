9b709348b2c095285d9ee3206933153e06a767bb
Antony Southworth <southworthy@gmail.com>
2017-05-22 06:22:51 +1200

    Added gitignore. Python version takes "*.text" and outputs "*.html"

--- python/textify.py
+++ python/textify.py
@@ -2,42 +2,62 @@ import argparse
 
 def main():
     # process args
-    argparser = argparse.ArgumentParser(description="Preprocesses textify")
+#{
+    # at the moment there is only one argument:
+    # --file FILE : The file to be processed
+    argparser = argparse.ArgumentParser(description="Processes textify files")
     argparser.add_argument(
         "--file",
         type=open,
         help="The file to be processed")
-    args = argparser.parse_args()
 
+    # TODO Add check for if nothing was passed to skip
+    args = argparser.parse_args()
+#}
+
+    # define counter
+#{
+    # There is one counter per "level" of section (e.g 1.2.3)
+    # We inc/decrement the section level when we enter or leave a section, respectively.
     sectioncounter = [0, 0, 0, 0]
     sectionlevel = 0
+#}
 
     # process file
+#{
     for line in args.file:
 
-        # check for what rule applies
-        # write result line to new file
-
         if "<section>" in line:
+        # Increment section counter for this level, and go up one level
             sectioncounter[sectionlevel] += 1
             sectionlevel += 1
 
         if "</section>" in line:
+        # reset section counter for this level and go down one level
             sectioncounter[sectionlevel] = 0
             sectionlevel -= 1
 
         numberstr = str(sectioncounter[0])
+        # we always want the top-level section number
 
         for num in sectioncounter[1:]:
+        # append sub-section numbers if they are not zero
             if num != 0:
                 numberstr += f".{num}"
 
         line = line.replace("<sectiontitle>", "<sectiontitle>"+numberstr+": ", 1)
+        # prepend section number to section title
+#}
 
-        with open(str(args.file)+".html", "a") as output:
+    # write output
+#{
+        outfilename = args.file.name.replace(".text", ".html", 1)
+        with open(outfilename, "a") as output:
             output.write(line)
+#}
 
     return 0
+    # end main
 
 if __name__ == "__main__":
     main()
